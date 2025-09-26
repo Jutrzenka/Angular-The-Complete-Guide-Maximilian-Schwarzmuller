ae146281dfd97c563e3492223f6648509d84d356
Kacper Czaja <kacperczaja@duck.com>
2025-09-27 01:31:43 +0200

    Create dynamic user component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.scss'
+  styleUrl: './user.scss',
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
