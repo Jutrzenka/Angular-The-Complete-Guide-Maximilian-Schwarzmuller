cb8d4f4e8e4c85badc217c596faf894e8961338d
Kacper Czaja <kacperczaja@duck.com>
2025-09-28 00:39:41 +0200

    Created model.ts and export interface into this files

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,6 @@
 // import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Component, input, output, computed } from '@angular/core';
-
-interface UserInterface {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { UserInterface } from './user.model';
 
 @Component({
   selector: 'app-user',
